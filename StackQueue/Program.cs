17dda3670bc0381c56df5a7d76cd24705120b97b
konikasingh <konikasingh1996@gmail.com>
2021-10-29 00:18:10 +0530

    Added Enqueue Program

--- StackQueue/Program.cs
+++ StackQueue/Program.cs
@@ -6,16 +6,26 @@ namespace StackQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n Stack operation Push and Pop:");
-            UC1StackPushPop stk = new UC1StackPushPop();
-            stk.push(56);
-            stk.push(30);
-            stk.push(70);
+            //Console.WriteLine("\n Stack operation Push and Pop:");
+            //UC1StackPushPop stk = new UC1StackPushPop();
+            //stk.push(56);
+            //stk.push(30);
+            //stk.push(70);
+            //stk.display();
+            //stk.peek();
+            //stk.pop();
+            //stk.IsEmpty();
+            //stk.display();
+
+            Console.WriteLine("\n Queue operation Enqueue and Dequeue:");
+            UC2QueueOperations stk = new UC2QueueOperations();
+            stk.Enqueue(56);
+            stk.Enqueue(30);
+            stk.Enqueue(70);
             stk.display();
-            stk.peek();
-            stk.pop();
-            stk.IsEmpty();
-            stk.display();
+            //stk.Dequeue();
+            //stk.display();
+
         }
     }
 }
---
+++ StackQueue/UC2QueueOperations.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    public  class UC2QueueOperations
+    {
+        Node head = null;
+        internal void Enqueue(int data) //inserting element from begining
+        {
+            Node node = new Node(data);
+
+            // If queue is empty, then new node is front and rear both  
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+
+        //internal void Dequeue() //deleting element from begining
+        //{
+        //    Node node = new Node();
+
+        //    if (this.head == null)
+        //    {
+        //        Console.WriteLine("no elements present ");
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        Node temp = head;
+               
+        //        while (this.head != null)
+        //        {
+        //            this.head = this.head.next;
+        //           // this.head.next = node;
+        //       // Console.WriteLine("hello", temp);
+        //        }
+        //        this.head.next = node;
+        //        //temp.next = null;
+        //       // Console.WriteLine(" {0} elements in queue are:", temp);
+        //    }
+        //    //this.head = temp.next;
+        //    //temp = null;
+        //    Console.WriteLine(" {0} elements in queue are:", node.data);
+        //}
+        internal void display()  //display the element of queue
+        {
+            Node temp = this.head;
+            if (temp == null) //checking the front and rear is null or not
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null) //untill fornt and rear not equal then pro
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
